d6b446c3c3e1a28d4f56af2cb563da9c6e2627ae
indiaprince <21600767@handong.edu>
2022-04-20 15:09:38 +0900

    decompose

--- CompoNDecompo/decompose.py
+++ CompoNDecompo/decompose.py
@@ -1,15 +1,20 @@
-import Alphabets
-
-
+from .Alphabets import HEAD,TAIL,BODY,FIRST_KOREAN_UNICODE
 def Decompose(text):
     decom_list = []
-    for t in text:
-        if(ord(t)==32) : 
-            decom_list.append('')
+    for ch in text:
+        Uni_Value = ord(ch)
+        if(Uni_Value==32) : 
+            decom_list.append(('',[[]]))
             continue
-        one = ord(t) - Alphabets.FIRST_KOREAN_UNICODE
-        decom_list.append(Alphabets.HEAD[one//(21*28)])
-        decom_list.append(Alphabets.BODY[one//28%21])
-        decom_list.append(Alphabets.TAIL[one%28]) 
+        if(Uni_Value==46) : 
+            decom_list.append(('.',[[]]))
+            continue
+        Uni_Korean = Uni_Value - FIRST_KOREAN_UNICODE
+        Uni_Head = Uni_Korean//(21*28)
+        Uni_Body = Uni_Korean//28%21
+        Uni_Tail = Uni_Korean%28
+        decom_list.append(HEAD[Uni_Head])
+        decom_list.append(BODY[Uni_Body])
+        decom_list.append(TAIL[Uni_Tail]) 
     return decom_list
 
