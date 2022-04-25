0dd42f4c0eefdee4056c7ff9824b7460ef54c2c6
indiaprince <21600767@handong.edu>
2022-04-25 11:30:43 +0900

    qna page and posting_css fixed

--- Qna/views.py
+++ Qna/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
+from .models import Qna_Posting, Qna_Chatting
+from User.models import User
+from django.core.paginator import Paginator
+#from .forms import PostForm
 
-# Create your views here.
-def index(request):
-    return render(request,'../templates/qna.html')
+def qna(request):
+    qna_list = Qna_Posting.objects.all()
+    now_page =int(request.GET.get('page', 1))
+    qna_list = qna_list.order_by('-qna_idx')
+                # 포스트 , 보여줄 게시글 개수
+    p = Paginator(qna_list, 10)
+    info = p.get_page(now_page)
+
+    # start_page = (now_page - 1) // 3 * 3 + 1
+    # end_page = start_page + 3
+    # if end_page > p.num_pages:
+    #     end_page = p.num_pages
+
+    # 페이지 마지막 번호
+    last_page_num = 0
+    for last_page in p.page_range:
+        last_page_num = last_page 
+
+
+
+    context = {
+        'info' : info,
+        'now_page' : now_page,
+        'last_page_num' : last_page_num
+    }
+    return render(request, '../templates/qna.html', context)
+
+
+def qna_edit():
+    return
+
+def qna_detail():
+    return
